b8f197fd6341f4e999af1d97dc324b0f19a52b8f
Nathan Debaz <nathandebaz266@gmail.com>
2021-08-16 12:20:43 -0300

    2 feito

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'landing', to: 'pages#landing'
+  get 'answer', to: 'pages#answer'
 end
